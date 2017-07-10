b084415921aae625e32564f2ca4d9218e62bf28f
Boyer <julien@meetserious.com>
2017-07-10 08:00:10 +0200

    add changes tests

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="main">
       <h3>Page principale</h3>
+      <p id="paraph">Je suis la page</p>
+      <div class="box">
+        <h4>Je suis dans la boite</h4>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'main',
+  data(){
+    visibility: false
+  },
+  created(){
+    
+  }
 }
 </script>
--- test/unit/specs/Main.spec.js
+++ test/unit/specs/Main.spec.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
-import Hello from '@/components/Main'
+import Main from '@/components/Main'
 
+
+/**
+ * https://vuejs.org/v2/guide/unit-testing.html
+ */
 describe('Main.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
+    const Constructor = Vue.extend(Main)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.main h3').textContent)
       .to.equal('Page principale')
+    expect(vm.$el.querySelector('.main p#paraph').textContent)
+      .to.equal('Je suis la page')
+    expect(vm.$el.querySelector('.main .box h4').textContent)
+      .to.equal('Je suis dans la boite')
+
+  })
+
+  it('has a created hook', () => {
+    console.log(Main)
+    expect(typeof Main.created).toBe('function')
   })
 })
